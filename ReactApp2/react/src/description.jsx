2515d4177308ed8a776512fd0f16c2dc3ba1d9cd
Bittu Raj <bitturaj124365@gmail.com>
2025-08-23 23:40:12 +0530

    First commit - project setup

---
+++ ReactApp2/react/src/description.jsx
@@ -0,0 +1,11 @@
+import "./Description.css";
+function Description(){
+    return(
+        <>
+        <div className="description">
+            <h2>Hii, I'm the description.</h2>
+        </div>
+        </>
+    );
+}
+export default Description;
