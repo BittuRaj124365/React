2c497bd4f3a97ff9b858c6a00a43ba167f0660ee
Bittu Raj <bitturaj124365@gmail.com>
2025-08-25 20:51:18 +0530

    worked on tailwind and props:

---
+++ Props/tailwindProps/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Cards from './Components/Cards'
+
+function App() {
+
+  return (
+    <>
+  <h1 className="bg-yellow-400 p-4 rounded-xl text-black">Tailwind Testing</h1>
+    <h1>Hello world</h1>
+    <Cards/>
+    <Cards/>
+    </>
+  )
+}
+
+export default App
