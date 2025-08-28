f52185e468e36ec1145fbe74b5c5463434a98667
Bittu Raj <bitturaj124365@gmail.com>
2025-08-28 13:19:28 +0530

    worked on bgChanger Project:

--- basicProjects/backgroundChanger/src/App.jsx
+++ basicProjects/backgroundChanger/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 function App() {
-  const [color, setColor] = useState("red");
+  const [color, setColor] = useState("olive");
 
   return (
     <>
@@ -10,10 +10,44 @@ function App() {
         style={{ backgroundColor: color, height: "100vh", width: "100%" }}
       >
         <div className="fixed bottom-12 inset-x-0 flex justify-center">
-          <button >Red</button>
+          <button onClick={()=>{setColor("red")}}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"style={{backgroundColor:"red"}}>Red</button>
         </div>
-        <div className="fixed bottom-12 inset-x-0 flex justify-center">
-          <button >Green</button>
+
+        {/* green */}
+        <div 
+        onClick={()=>{setColor("green")}}className="fixed bottom-12 inset-x-0 flex justify-center">
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg"style={{backgroundColor:"green"}}>Green</button>
+        </div>
+
+        {/* pink */}
+        <div onClick={()=>{setColor("pink")}}
+        className="fixed bottom-12 inset-x-0 flex justify-center">
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg"style={{backgroundColor:"pink"}}>Pink</button>
+        </div>
+
+        {/* white */}
+        <div onClick={()=>{setColor("white")}}
+        className="fixed bottom-12 inset-x-0 flex justify-center">
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg"style={{backgroundColor:"white"}}>White</button>
+        </div>
+
+        {/* yellow */}
+        <div onClick={()=>{setColor("yellow")}}
+        className="fixed bottom-12 inset-x-0 flex justify-center">
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg"style={{backgroundColor:"yellow"}}>Yellow</button>
+        </div>
+
+        {/* orange */}
+        <div onClick={()=>{setColor("orange")}}
+        className="fixed bottom-12 inset-x-0 flex justify-center">
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg"style={{backgroundColor:"orange"}}>Orange</button>
+        </div>
+
+        {/* black */}
+        <div onClick={()=>{setColor("cyan")}}
+        className="fixed bottom-12 inset-x-0 flex justify-center">
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg"style={{backgroundColor:"cyan"}}>cyan</button>
         </div>
       </div>
     </>
