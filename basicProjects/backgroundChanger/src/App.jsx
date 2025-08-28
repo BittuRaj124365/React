5336015c0cd083dadd87a5b191d3e632a182a7f5
Bittu Raj <bitturaj124365@gmail.com>
2025-08-28 12:38:35 +0530

    worked on bg changer

--- basicProjects/backgroundChanger/src/App.jsx
+++ basicProjects/backgroundChanger/src/App.jsx
@@ -1,19 +1,22 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 function App() {
-  const [color, setColor]=useState("Yellow")
+  const [color, setColor] = useState("red");
+
   return (
     <>
-    <div className="w-full h-screen duration-400" 
-    style={{backgroundColor:color, height: "100vh", width: "100%" }}>
-      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2 flex justify-center ">
-        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-xl ">
+      <div
+        className="w-full h-screen"
+        style={{ backgroundColor: color, height: "100vh", width: "100%" }}
+      >
+        <div className="fixed bottom-12 inset-x-0 flex justify-center">
           <button >Red</button>
         </div>
+        <div className="fixed bottom-12 inset-x-0 flex justify-center">
+          <button >Green</button>
+        </div>
       </div>
-    </div>
     </>
-  )
+  );
 }
-
-export default App
+export default App;
