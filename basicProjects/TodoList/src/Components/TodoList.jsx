a3a7f548d1da6e47e1e4adefaed5994370dde6f3
Bittu Raj <bitturaj124365@gmail.com>
2025-08-31 22:45:51 +0530

    worked on toDo list's upperCase useState

--- basicProjects/TodoList/src/Components/TodoList.jsx
+++ basicProjects/TodoList/src/Components/TodoList.jsx
@@ -8,17 +8,41 @@ export default function TodoList() {
     setTodo((preTodo) => {
       return [...preTodo, { task: newTodo, id: uuidv4() }];
     });
-    setNewTodo(""); //to make the input box input after adding
+    setNewTodo(""); //to make the input box empty after adding
   };
   let updateTodoValue = (event) => {
     // console.log(event.target.value)
     setNewTodo(event.target.value);
   };
   // to delete todo
-  let deleteTodo=(id)=>{
-    setTodo((preTodo)=>todo.filter((preTodo)=>preTodo.id!=id));
+  let deleteTodo = (id) => {
+    setTodo((preTodo) => todo.filter((preTodo) => preTodo.id != id));
     // console.log(id);
-  }
+  };
+  let upperCaseAll = () => {
+    setTodo(
+      todo.map((todo) => {
+        return {
+          ...todo,
+          task: todo.task.toUpperCase(),
+        };
+      })
+    );
+  };
+  let upperCaseOne = (id) => {
+    setTodo((todo) =>
+      todo.map((todo) => {
+        if (todo.id == id) {
+          return {
+            ...todo,
+            task: todo.task.toUpperCase(),
+          };
+        } else {
+          return todo;
+        }
+      })
+    );
+  };
   return (
     <div>
       <h2>toDo list</h2>
@@ -37,9 +61,20 @@ export default function TodoList() {
           <li key={todo.id}>
             <span> {todo.task}</span>
             &nbsp; &nbsp;&nbsp;
-            <button onClick={()=>{deleteTodo(todo.id)}}>Delete</button>
+            <button
+              onClick={() => {
+                deleteTodo(todo.id);
+              }}
+            >
+              Delete
+            </button>
+            <button onClick={() => upperCaseOne(todo.id)}>UpperCaseOne</button>
           </li>
         ))}
       </ul>
+      <br />
+      <br />
+      <button onClick={upperCaseAll}>UpperCase</button>
     </div>
-)}
+  );
+}
