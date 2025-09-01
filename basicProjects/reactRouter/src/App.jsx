1f10437c3d0745b98489e51e44b7634bdfb93192
Bittu Raj <bitturaj124365@gmail.com>
2025-09-01 23:49:02 +0530

    startworking on reactRouter

--- basicProjects/reactRouter/src/App.jsx
+++ basicProjects/reactRouter/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './Components/Header/Header.jsx'
 
 function App() {
 
   return (
     <>
-      <h1 className=' text-cran-499'>React Routing</h1>
+      <h1 className='text-red-500 p-4'>react routing</h1>
+      <Header/>
     </>
   ) 
 }
