0d622bcec70e94f80e089f81d95f1994af4dce18
Bittu Raj <bitturaj124365@gmail.com>
2025-10-01 17:00:16 +0530

    Started crytp dashboard with chakra ui

--- MiniProjects/Crypto Dashboard/src/App.jsx
+++ MiniProjects/Crypto Dashboard/src/App.jsx
@@ -1,15 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import Sidenav from "./Component/Sidenav";
 function App() {
-
   return (
     <>
-      <h1>Cryptp Dashboard</h1>
+      <Sidenav/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ MiniProjects/Crypto Dashboard/src/Component/Sidenav.jsx
@@ -0,0 +1,10 @@
+import { Box, Button } from "@chakra-ui/react";
+
+export default function Sidenav() {
+  return (
+    <>
+      <Box>hiiii</Box>
+      <Button>Click me </Button>
+    </>
+  );
+}
