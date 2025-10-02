fb62a44dfa890eb0d8e9ceea92c0d057445d377c
Bittu Raj <bitturaj124365@gmail.com>
2025-10-02 11:20:32 +0530

    there is some error related to export some modules in Chakra ui which is not letting me move forward and causing the error

--- MiniProjects/Crypto Dashboard/src/Component/Topnav.jsx
+++ MiniProjects/Crypto Dashboard/src/Component/Topnav.jsx
@@ -1,14 +1,5 @@
-import {
-  Button,
-  Container,
-  Flex,
-  Heading,
-  Menu,
-  MenuList,
-  MenuItem,
-  MenuButton
-} from "@chakra-ui/react";
 import React from "react";
+import { Button,Container,Flex, Heading,Menu, MenuList,MenuItem,MenuButton} from "@chakra-ui/react";
 const Topnav = () => {
   return (
     <Flex boxShadow="xl">
